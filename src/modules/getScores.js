cae4f2ce7d3860761173594ca685dfb4a99165c9
Dennis <vigehi2017@gmail.com>
2022-09-02 11:30:46 +0300

    Fix linters

--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -1,11 +1,13 @@
 async function sendAPi(arg1, arg2) {
-    const send = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/p5yqiCio7d4FE7KGl6s2/scores';
-    fetch(send, {
-        method: 'POST', 
-        body: JSON.stringify({ 
-            user: arg1, score: arg2, }),
-        headers: { 
-            'Content-type': 'application/json; charset=UTF-8', },
+    const receive = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/p5yqiCio7d4FE7KGl6s2/scores';
+    fetch(receive, {
+        method: 'POST',
+        body: JSON.stringify({
+            user: arg1, score: arg2,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
     }).then((param) => param.json()).then((saved) => saved);
 }
 export default sendAPi; 
